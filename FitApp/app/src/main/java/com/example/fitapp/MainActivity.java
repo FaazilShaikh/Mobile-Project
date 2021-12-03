566e5f21d6664d3343d59feab76d4a2f6dafc0ac
FaazilShaikh <faazil.shaikh@ontariotechu.net>
2021-12-03 13:19:00 -0500

    Updated UI

--- FitApp/app/src/main/java/com/example/fitapp/MainActivity.java
+++ FitApp/app/src/main/java/com/example/fitapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.fitapp;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import androidx.appcompat.widget.AppCompatButton;
 
@@ -8,29 +12,52 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import com.example.fitapp.databinding.ActivityMainBinding;
-
-public class MainActivity extends AppCompatActivity {
+import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+
+    private TextView stepCount;
+    private SensorManager sensorManager;
+    CircularProgressBar circularProgressBar;
+    private Sensor mStepCounter;
+    private boolean isSensorActive;
+    private float numberSteps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b = findViewById(R.id.button);
-        /* Uncomment and change intent here to go to other pages you guys have added
-        b.setOnClickListener(v -> startActivity(new Intent(
-                getApplicationContext(), MapsActivity.class))
-        );
-        */
+        circularProgressBar = findViewById(R.id.circularProgressBar);
 
+        Button b = findViewById(R.id.button);
+
+        b.setOnClickListener(v -> startActivity(new Intent(
+                getApplicationContext(), songActivity.class))
+        );
+        Button b3=findViewById(R.id.button3);
+        b3.setOnClickListener(v-> startActivity(new Intent(
+                getApplicationContext(),userInfostats.class
+        )));
+
+        stepCount = findViewById(R.id.stepDisplay);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null) {
+            mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isSensorActive = true;
+            stepCount.setText("Steps: 0");
+        }else{
+            stepCount.setText("Steps: 0");
+            isSensorActive = false;
+        }
     }
 
     @Override
@@ -53,4 +80,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!= null){
+            sensorManager.registerListener(this, mStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!= null){
+            sensorManager.unregisterListener(this, mStepCounter);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor==mStepCounter){
+            numberSteps = event.values[0];
+            stepCount.setText("Steps: "+String.valueOf(numberSteps));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
